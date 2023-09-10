6f623d7ecbfe854a89984e17f469a8323940c0ad
saleemaskrea <144629807+saleemaskrea@users.noreply.github.com>
2023-09-10 18:32:47 -0400

    Add files via upload

---
+++ week01/ex1.sh
@@ -0,0 +1,4 @@
+mkdir ∼/week01
+cd ∼/week01
+find /usr/bin -type f -name "*gcc*" | sort -r | tail -n 5 > ~/week01/ex1.txt
+
---
+++ week01/ex3.sh
@@ -0,0 +1,18 @@
+printDateandsleep(){
+date 
+sleep 3
+}
+mkdir root_folder
+printDateandsleep
+mkdir home_folder
+printDateandsleep
+touch root_folder/root.txt
+printDateandsleep
+touch home_folder/home.txt
+printDateandsleep
+ls -l
+chmod +xrw root.txt
+ls -lt -r / > root.txt
+ls -lt -r ~ > home.txt
+cat root_folder/root.txt
+cat home_folder/home.txt
